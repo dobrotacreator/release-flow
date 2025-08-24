dd2c061f053457ae14869334f26517284f9b1ac5
Igor Pavlyukovets <igor.p.i@yandex.ru>
2025-08-24 22:09:48 +0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/release-flow" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/release-flow/" : "",
 };
 
 export default nextConfig;
