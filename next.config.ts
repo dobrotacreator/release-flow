2735bf91dcbe54f79777ec70cc725064fd070735
Igor Pavlyukovets <45421346+dobrotacreator@users.noreply.github.com>
2025-11-01 12:36:19 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/release-flow" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/release-flow/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/pet_release-flow" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/pet_release-flow/" : "",
 };
 
 export default nextConfig;
